fbedd4ed5d9592f420a2b747a11b21cdb53896ed
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-04-27 11:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let color = document.querySelector('#colorSelect');
+let btn = document.getElementsByTagName('input')[0];
+
+btn.addEventListener("click", () =>{
+	let option = document.getElementsByTagName('option');
+	for(let t of option){
+		if(t.innerText === color.value){
+			return color.removeChild(t);
+		}
+	}
+})
